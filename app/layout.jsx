2f21086989e70acf618ac5c69606268fa05df453
nimavisker1360 <nimabaghery@gmail.com>
2024-09-05 14:41:29 +0300

    property

---
+++ app/layout.jsx
@@ -0,0 +1,22 @@
+import "@/assets/styles/globals.css";
+import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
+export const metadata = {
+  title: "Property Pulse",
+  keywords: "rental,property,real estate",
+  description: "Find the perfect rental property",
+};
+
+const MainLayout = ({ children }) => {
+  return (
+    <html>
+      <body>
+        <Navbar />
+        <main>{children}</main>
+        <Footer />
+      </body>
+    </html>
+  );
+};
+
+export default MainLayout;
---
+++ app/page.jsx
@@ -0,0 +1,13 @@
+import Hero from "@/components/Hero";
+import InfoBoxes from "@/components/InfoBoxes";
+
+const HomePage = () => {
+  return (
+    <div>
+      <Hero />
+      <InfoBoxes />
+    </div>
+  );
+};
+
+export default HomePage;
