cfac67a824bc56ca152d54329ffd305f7d0b984f
nimavisker1360 <nimabaghery@gmail.com>
2024-10-11 16:10:15 +0300

    hot fix

--- app/layout.jsx
+++ app/layout.jsx
@@ -3,7 +3,9 @@ import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 import AuthProvider from "@/components/AuthProvider";
 import { ToastContainer } from "react-toastify";
+import { GlobalProvider } from "@/context/GlobalContext";
 import "react-toastify/dist/ReactToastify.css";
+import "photoswipe/dist/photoswipe.css";
 
 export const metadata = {
   title: "Property Pulse",
@@ -14,14 +16,16 @@ export const metadata = {
 const MainLayout = ({ children }) => {
   return (
     <AuthProvider>
-      <html>
-        <body>
-          <Navbar />
-          <main>{children}</main>
-          <Footer />
-          <ToastContainer />
-        </body>
-      </html>
+      <GlobalProvider>
+        <html>
+          <body>
+            <Navbar />
+            <main>{children}</main>
+            <Footer />
+            <ToastContainer />
+          </body>
+        </html>
+      </GlobalProvider>
     </AuthProvider>
   );
 };
--- components/MessageCard.jsx
+++ components/MessageCard.jsx
@@ -1,14 +1,51 @@
-import { Span } from "next/dist/trace";
+"use client";
+import { useState } from "react";
+import { toast } from "react-toastify";
+import markMessageAsRead from "@/app/actions/markMessageAsRead";
+import deleteMessage from "@/app/actions/deleteMessage";
+import { useGlobalContext } from "@/context/GlobalContext";
 
 const MessageCard = ({ message }) => {
+  const [isRead, setIsRead] = useState(message.read);
+  const [isDeleted, setIsDeleted] = useState(false);
+
+  const { setUnreadCount } = useGlobalContext();
+
+  const handelDeleteClick = async () => {
+    await deleteMessage(message._id);
+    setIsDeleted(true);
+    setUnreadCount((prevCount) => (isRead ? prevCount : prevCount - 1));
+    toast.success("Message Deleted");
+  };
+  if (isDeleted) {
+    return <p>Deleted Message</p>;
+  }
+
+  const handleReadClick = async () => {
+    const read = await markMessageAsRead(message._id);
+    setIsRead(read);
+    setUnreadCount((prevCount) => (read ? prevCount - 1 : prevCount + 1));
+    toast.success(`Marked As ${read ? "Read" : "New"}`);
+  };
   return (
     <div className="relative bg-white p-4 rounded-md shadow-md border border-gray-200">
+      {!isRead && (
+        <div className="absolute top-2 right-2 bg-yellow-500 text-white px-2 py-1 rounded-md">
+          New
+        </div>
+      )}
       <h2 className="text-xl mb-4">
-        <Span className="font-bold">Property Inquiry:</Span>{" "}
+        <span className="font-bold">Property Inquiry:</span>{" "}
         {message.property.name}
       </h2>
       <p className="text-gray-700">{message.body}</p>
       <ul className="mt-4">
+        <li>
+          <strong>Reply Email</strong>{" "}
+          <a href={`tel:${message.phone}`} className="text-blue-500">
+            {message.email}
+          </a>
+        </li>
         <li>
           <strong>Reply Email</strong>{" "}
           <a href={`tel:${message.phone}`} className="text-blue-500">
@@ -26,10 +63,16 @@ const MessageCard = ({ message }) => {
           {new Date(message.createdAt).toLocaleString()}
         </li>
       </ul>
-      <button className="mt-4 mr-3 bg-blue-500 text-white py-1 px-3 rounded-md">
-        Mark as Read
+      <button
+        onClick={handleReadClick}
+        className="mt-4 mr-3 bg-blue-500 text-white py-1 px-3 rounded-md"
+      >
+        {isRead ? "Mark As New" : "Mark As Read"}
       </button>
-      <button className="mt-4 mr-3 bg-red-500 text-white py-1 px-3 rounded-md">
+      <button
+        onClick={handelDeleteClick}
+        className="mt-4 mr-3 bg-red-500 text-white py-1 px-3 rounded-md"
+      >
         Delete
       </button>
     </div>
